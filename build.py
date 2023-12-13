e21e1d46fdcb47e0baa7170566c253e69d42d090
Eliot W. Robson <eliot.robson24@gmail.com>
2023-12-12 22:46:12 -0500

    Changed build protocol

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import os
+#import os
 import shutil
 from pathlib import Path
 
@@ -8,64 +8,38 @@ from pathlib import Path
 
 from setuptools import Extension  # noqa: I001
 from setuptools.dist import Distribution  # noqa: I001
-import Cython.Compiler.Options  # pyright: ignore [reportMissingImports]
+#import Cython.Compiler.Options  # pyright: ignore [reportMissingImports]
 from Cython.Build import build_ext, cythonize  # pyright: ignore [reportMissingImports]
 
-EXTENSIONS = [
-    Extension('leafy.data_structure', ['leafy/data_structure.pyx']),
-    Extension('leafy.graph', ['leafy/graph.pyx']),
-    Extension('leafy.search', ['leafy/search.pyx']),
-    Extension('leafy.digraph', ['leafy/digraph.pyx']),
-    Extension('leafy.shortest_path', ['leafy/shortest_path.pyx']),
-]
+#EXTENSIONS = [
+#    Extension('leafy.data_structure', ['leafy/data_structure.pyx']),
+#    Extension('leafy.graph', ['leafy/graph.pyx']),
+#    Extension('leafy.search', ['leafy/search.pyx']),
+#    Extension('leafy.digraph', ['leafy/digraph.pyx']),
+#    Extension('leafy.shortest_path', ['leafy/shortest_path.pyx']),
+#]
 
 
 def build_cython_extensions():
-
-    Cython.Compiler.Options.annotate = True
-
-    if os.name == "nt":  # Windows
-        extra_compile_args = [
-            "/O2",
-        ]
-    else:  # UNIX-based systems
-        extra_compile_args = [
-            "-O3",
-            "-Werror",
-            "-Wno-unreachable-code-fallthrough",
-            "-Wno-deprecated-declarations",
-            "-Wno-parentheses-equality",
-            "-Wno-unreachable-code",  # TODO: This should no longer be necessary with Cython>=3.0.3
-        ]
-    extra_compile_args.append("-UNDEBUG")  # Cython disables asserts by default.
-    # Relative to project root director
-    include_dirs = [
-        "pythontemplate/",
-        "pythontemplate/_c_src",
-    ]
-
-    c_files = [str(x) for x in Path("pythontemplate/_c_src").rglob("*.c")]
     extensions = [
-        Extension(
-            # Your .pyx file will be available to cpython at this location.
-            "pythontemplate._c_extension",
-            [
-                # ".c" and ".pyx" source file paths
-                "pythontemplate/_c_extension.pyx",
-                *c_files,
-            ],
-            include_dirs=include_dirs,
-            extra_compile_args=extra_compile_args,
-            language="c",
-        ),
+        Extension('leafy.data_structure', ['leafy/data_structure.pyx']),
+        Extension('leafy.graph', ['leafy/graph.pyx']),
+        Extension('leafy.search', ['leafy/search.pyx']),
+        Extension('leafy.digraph', ['leafy/digraph.pyx']),
+        Extension('leafy.shortest_path', ['leafy/shortest_path.pyx']),
     ]
 
-    include_dirs = set()
-    for extension in extensions:
-        include_dirs.update(extension.include_dirs)
-    include_dirs = list(include_dirs)
+    #include_dirs = set()
+    #for extension in extensions:
+    #    include_dirs.update(extension.include_dirs)
+    #include_dirs = list(include_dirs)
 
-    ext_modules = cythonize(extensions, include_path=include_dirs, language_level=3, annotate=True)
+    ext_modules = cythonize(
+        extensions,
+        annotate=True,
+        compiler_directives={'language_level': "3str"}
+    )
+
     dist = Distribution({"ext_modules": ext_modules})
     cmd = build_ext(dist)
     cmd.ensure_finalized()
