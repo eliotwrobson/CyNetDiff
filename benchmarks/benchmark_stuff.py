b260b9c2a16ad521f5e37bc25dcca3f4f7a43e8a
Eliot Robson <eliot.robson24@gmail.com>
2024-03-09 14:41:07 -0600

    Update benchmark script

--- benchmarks/benchmark_stuff.py
+++ benchmarks/benchmark_stuff.py
@@ -1,5 +1,7 @@
+import itertools as it
 import random
 import time
+import typing as t
 
 import ndlib.models.epidemics as ep
 import ndlib.models.ModelConfig as mc
@@ -8,8 +10,16 @@ import pandas as pd
 from cynetdiff.utils import networkx_to_ic_model
 from tqdm import trange
 
+DiffusionGraphT = nx.Graph | nx.DiGraph
+SeedSetT = set[int]
+DiffusionFuncT = t.Callable[[DiffusionGraphT, SeedSetT, int], float]
 
-def independent_cascade(G: nx.Graph | nx.DiGraph, seeds: list[int]) -> list[list[int]]:
+
+def independent_cascade(G: DiffusionGraphT, seeds: SeedSetT) -> list[list[int]]:
+    """
+    A basic pure-python implementation of independent cascade. Optimized so we can get
+    a baseline reading on the best possible speed for this algorithm in pure-Python.
+    """
     if not G.is_directed():
         G = G.to_directed()
 
@@ -18,7 +28,7 @@ def independent_cascade(G: nx.Graph | nx.DiGraph, seeds: list[int]) -> list[list
     # this is basically BFS, except that the current layer only stores the nodes at
     # same distance from sources at each iteration
     res = []
-    current_layer = seeds.copy()
+    current_layer = list(seeds)
 
     while current_layer:
         res.append(current_layer)
@@ -40,10 +50,10 @@ def independent_cascade(G: nx.Graph | nx.DiGraph, seeds: list[int]) -> list[list
     return res
 
 
-### TODO ignore the crap above
-def diffuse_python(
-    graph: nx.Graph | nx.DiGraph, seeds: set[int], num_samples: int
-) -> float:
+def diffuse_python(graph: DiffusionGraphT, seeds: SeedSetT, num_samples: int) -> float:
+    if not graph.is_directed():
+        graph = graph.to_directed()
+
     res = 0.0
     seeds_list = list(seeds)
     for _ in trange(num_samples):
@@ -53,7 +63,7 @@ def diffuse_python(
 
 
 def diffuse_CyNetDiff(
-    graph: nx.Graph | nx.DiGraph, seeds: set[int], num_samples: int
+    graph: DiffusionGraphT, seeds: SeedSetT, num_samples: int
 ) -> float:
     model = networkx_to_ic_model(graph)
     model.set_seeds(seeds)
@@ -68,9 +78,7 @@ def diffuse_CyNetDiff(
     return total_activated / num_samples
 
 
-def diffuse_ndlib(
-    graph: nx.Graph | nx.DiGraph, seeds: set[int], num_samples: int
-) -> float:
+def diffuse_ndlib(graph: DiffusionGraphT, seeds: SeedSetT, num_samples: int) -> float:
     model = ep.IndependentCascadesModel(graph)
 
     config = mc.Configuration()
@@ -78,8 +86,9 @@ def diffuse_ndlib(
     # Assume that thresholds were already set.
     for u, v, data in graph.edges(data=True):
         config.add_edge_configuration("threshold", (u, v), data["threshold"])
-    # print(seeds)
-    # config.add_model_parameter("infected", seeds)
+
+    # Don't randomly infect anyone to start, just use given seeds.
+    config.add_model_parameter("fraction_infected", 0.0)
 
     model.set_initial_status(config)
     total_infected = 0.0
@@ -98,53 +107,75 @@ def diffuse_ndlib(
     return total_infected / num_samples
 
 
-def time_diffusion(func, graph, seeds, num_samples):
+def time_diffusion(
+    func: DiffusionFuncT, graph: DiffusionGraphT, seeds: SeedSetT, num_samples: int
+):
     start = time.perf_counter()
     diffused = func(graph, seeds, num_samples)
     end = time.perf_counter()
     return func.__name__, diffused, end - start
 
 
+def get_graphs() -> list[tuple[str, DiffusionGraphT]]:
+    """Get graphs with accompanying names for diffusion benchmarks."""
+    n_values = [20, 10_000]
+    frac_values = [0.002, 0.007]
+    threshold_values = [0.1]
+
+    res = []
+
+    for n, frac, threshold in it.product(n_values, frac_values, threshold_values):
+        graph = nx.fast_gnp_random_graph(n, frac)
+
+        nx.set_edge_attributes(graph, threshold, "threshold")
+
+        name = f"Gnp, n = {n}, p = {frac}"
+
+        res.append((name, graph))
+
+    return res
+
+
 def main() -> None:
     # TODO parameterize these benchmarks with
     # https://networkx.org/documentation/stable/reference/generators.html#module-networkx.generators.random_graphs
 
-    n_values = [10_000]
-    k_values = [1, 2, 5, 10, 20]
-    frac_values = [0.002, 0.007]
-    num_samples_values = [1000]
-    diffusion_functions = [diffuse_CyNetDiff, diffuse_ndlib, diffuse_python]
+    seed_values = [1, 2, 5, 10, 20]
+    num_samples_values = [10, 1000]
+    diffusion_functions = [diffuse_CyNetDiff, diffuse_python, diffuse_ndlib]
+
+    underlying_graphs = get_graphs()
 
     results = []
 
-    for n, k, frac, num_samples in zip(
-        n_values, k_values, frac_values, num_samples_values
+    for (graph_name, graph), num_seeds, num_samples in it.product(
+        underlying_graphs, seed_values, num_samples_values
     ):
-        g = nx.fast_gnp_random_graph(n, frac)
-        nx.set_edge_attributes(g, 0.1, "threshold")
-        seeds = set(random.sample(list(g.nodes()), k))
+        seeds = set(random.sample(list(graph.nodes()), num_seeds))
 
         for func in diffusion_functions:
             # Print the parameters before running
             print(
-                f"Running {func.__name__} with n={n}, k={k}, frac={frac}, num_samples={num_samples}"
+                f"Running {func.__name__} on {graph_name} with {graph.number_of_nodes()} nodes, "
+                f"{graph.number_of_edges()} edges, {num_seeds} seeds, {num_samples} samples."
             )
 
             model_name, diffused, time_taken = time_diffusion(
-                func, g, seeds, num_samples
+                func, graph, seeds, num_samples
             )
 
             # Print the results after running
             print(
-                f"Completed {model_name}: Diffused = {diffused}, Time = {time_taken:.4f} seconds"
+                f"Completed {model_name}: Diffused = {diffused}, Time = {time_taken:.4f} seconds\n"
             )
 
             results.append(
                 {
-                    "n": n,
-                    "k": k,
-                    "frac": frac,
-                    "num_samples": num_samples,
+                    "graph name": graph_name,
+                    "nodes": graph.number_of_nodes(),
+                    "edges": graph.number_of_edges(),
+                    "num seeds": num_seeds,
+                    "num samples": num_samples,
                     "model": model_name,
                     "diffused": diffused,
                     "time": time_taken,
